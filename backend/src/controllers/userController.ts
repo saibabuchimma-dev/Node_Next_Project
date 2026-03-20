6ed3afb683b99222a1ddf5fcb022821a279df2de
saibabuchimma-dev <saibabuchimma@gmail.com>
2026-03-20 11:25:58 +0530

    Connected DB and Node.js and insert data, In controller connected to real time database

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,12 +1,16 @@
 import { Request, Response } from "express";
+import { pool } from "../config/db";
 
-export const getUser = (req: Request, res: Response) => {
-    res.json({ message: "User fetched successfully !" });
-}
+export const getUser = async (req: Request, res: Response) => {
+  const result = await pool.query("SELECT * FROM users");
+  res.json(result.rows);
+};
 
-export const createUser = (req: Request, res: Response) => {
-    const {name, age} = req.body;
-    res.json({
-        message: `User ${name} is ${age} years old`,
-    });
+export const createUser = async (req: Request, res: Response) => {
+  const { name, email, age } = req.body;
+  const result = await pool.query(
+    "INSERT INTO users (name, email, age) VALUES ($1, $2, $3) RETURNING *",
+    [name, email, age],
+  );
+  res.json(result.rows[0]);
 };
