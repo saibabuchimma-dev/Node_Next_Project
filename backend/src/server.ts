abb319eb81a6fcbf987b19598e4a14ebda274942
saibabuchimma-dev <saibabuchimma@gmail.com>
2026-03-21 16:49:31 +0530

    Bugs fixing

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,17 @@
 import express from "express";
+import cors from "cors";
 import userRoutes from "./routes/userRoutes";
 import taskRoutes from "./routes/taskRoutes";
 
 const app = express();
 const PORT = 5000;
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
+);
 
 //MiddleWare
 app.use(express.json());
@@ -12,6 +20,7 @@ app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/tasks", taskRoutes);
 
+//Server Start
 app.listen(PORT, () => {
   console.log(`Server running on Port ${PORT}`);
 });
